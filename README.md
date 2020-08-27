87599199d7716d07d41461cc5bd8ed02e59b9093
fosdick.io <67963637+fosdickio@users.noreply.github.com>
2020-08-27 18:57:06 +0200

    Adding TIL content to my GitHub profile's README.md.

--- README.md
+++ README.md
@@ -12,13 +12,31 @@
 
 <table>
 <tr>
-<td valign="top" width="100%">
+<td valign="top" width="50%">
 
 ### Recent Blog Posts
 <!-- Blog entries start -->
-- [Anomaly Detection in the Service Mesh](https://www.fosdick.io/2018/04/17/anomaly-detection-in-the-service-mesh.html) - 2018-04-17
+- [Anomaly Detection in the Service Mesh](https://www.fosdick.io/2018/04/17/anomaly-detection-in-the-service-mesh.html) (2018-04-17)
 <!-- Blog entries end -->
 View more blog posts on [fosdick.io](https://www.fosdick.io/).
+
+</td>
+
+<td valign="top" width="50%">
+
+### Recent TILs
+<!-- TILs start -->
+- [NumPy Snippets](https://github.com/fosdickio/til/blob/main/python/numpy-snippets.md) (2020-08-27)
+
+- [Python Snippets](https://github.com/fosdickio/til/blob/main/python/python-snippets.md) (2020-08-27)
+
+- [Pandas Snippets](https://github.com/fosdickio/til/blob/main/python/pandas-snippets.md) (2020-08-27)
+
+- [Git Snippets](https://github.com/fosdickio/til/blob/main/git/git-snippets.md) (2020-08-26)
+
+- [Kubernetes Snippets](https://github.com/fosdickio/til/blob/main/kubernetes/kubernetes-snippets.md) (2020-08-26)
+<!-- TILs end -->
+View more TILs in [the TIL repository](https://github.com/fosdickio/til).
 
 </td>
 </tr>
--- build_readme.py
+++ build_readme.py
@@ -1,10 +1,12 @@
 import feedparser
 import pathlib
 import re
+import requests
 
 
 ROOT_PATH = pathlib.Path(__file__).parent.resolve()
 FEED_URL = "https://www.fosdick.io/feed.xml"
+TIL_URL = "https://raw.githubusercontent.com/fosdickio/til/main/README.md"
 
 
 def replace_chunk(content, marker, chunk, inline=False):
@@ -18,8 +20,8 @@ def replace_chunk(content, marker, chunk, inline=False):
     return r.sub(chunk, content)
 
 
-def fetch_blog_entries():
-    entries = feedparser.parse(FEED_URL)["entries"]
+def fetch_blog_entries(url):
+    entries = feedparser.parse(url)["entries"]
     return [
         {
             "title": entry["title"],
@@ -30,14 +32,44 @@ def fetch_blog_entries():
     ]
 
 
+def fetch_til_entries(url):
+    response = requests.get(url, timeout=60)
+    tils = []
+    if response.status_code == 200:
+        response_text = str(response.text)
+        lines = response_text.replace("\n\n", "\n").split("<!-- TILs start -->")[1].split("<!-- TILs end -->")[0].split("\n")
+        for line in lines:
+            if line.startswith("- ["):
+                til = {
+                    'markdown': line,
+                    'title': line.strip('- [').split('])')[0].split('](')[0],
+                    'url': line.strip('- [').split('])')[0].split('](')[1].split(') (')[0],
+                    'date': line.strip(')').split(' (')[1]
+                }
+                tils.append(til)
+    else:
+        raise RuntimeError("TIL request return status was not 200.")
+
+    sorted_tils = sorted(tils, key=lambda k: k['date'], reverse=True)
+
+    return sorted_tils
+
+
 if __name__ == "__main__":
     readme = ROOT_PATH / "README.md"
     readme_contents = readme.open().read()
 
-    entries = fetch_blog_entries()[:5]
-    entries_md = "\n\n".join(
-        ["- [{title}]({url}) - {published}".format(**entry) for entry in entries]
+    blog_entries = fetch_blog_entries(FEED_URL)[:5]
+    blog_entries_md = "\n\n".join(
+        ["- [{title}]({url}) ({published})".format(**entry) for entry in blog_entries]
     )
-    rewritten = replace_chunk(readme_contents, "Blog entries", entries_md)
 
-    readme.open("w").write(rewritten)
+    til_entries = fetch_til_entries(TIL_URL)[:5]
+    til_entries_md = "\n\n".join(
+        ["- [{title}]({url}) ({date})".format(**entry) for entry in til_entries]
+    )
+
+    readme_contents = replace_chunk(readme_contents, "Blog entries", blog_entries_md)
+    readme_contents = replace_chunk(readme_contents, "TILs", til_entries_md)
+
+    readme.open("w").write(readme_contents)
